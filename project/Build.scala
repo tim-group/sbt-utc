15f8e6476be15d5bbaaf256b4fbeba31a3e16229
Brian Roberts <brian.roberts@timgroup.com>
2014-06-05 07:45:26 -0400

    Brian: Add version, publishTo and credential settings.

--- project/Build.scala
+++ project/Build.scala
@@ -10,9 +10,10 @@ object SbtUtcBuild extends Build with BuildExtra {
     .settings(sbtPlugin := true,
       organization := "com.timgroup",
       name := "sbt-utc",
-      version := "0.0.1",
+      version := "0.0." + System.getProperty("BUILD_NUMBER", "0-SNAPSHOT"),
       sbtVersion in Global := "0.13.0",
       scalaVersion in Global := "2.10.2",
       libraryDependencies ++= lib_jodatime
-    )
+    ).settings(publishTo := Some("TIM Group Repo" at "http://repo.youdevise.com:8081/nexus/content/repositories/yd-release-candidates"))
+     .settings(credentials += Credentials(new File("/etc/sbt/credentials")))
 }
