03d7994cf4874322d2021f44a8f382da671b7a75
Marc Siegel <marc.siegel@timgroup.com>
2014-11-03 14:48:07 -0500

    Marc: (cosmetic: whitespace and indentation)

--- project/Build.scala
+++ project/Build.scala
@@ -2,18 +2,24 @@ import sbt._
 import Keys._
 
 object SbtUtcBuild extends Build with BuildExtra {
+
   lazy val lib_jodatime = Seq(
-    "joda-time" % "joda-time" % "2.3" % "optional",
-    "org.joda" % "joda-convert" % "1.3.1" % "optional"
+    "joda-time" % "joda-time"    % "2.3"   % "optional",
+    "org.joda"  % "joda-convert" % "1.3.1" % "optional"
   )
+
   val sbtUtc = Project("sbt-utc", file("."))
-    .settings(sbtPlugin := true,
+    .settings(
+      sbtPlugin    := true,
       organization := "com.timgroup",
-      name := "sbt-utc",
-      version := "0.0." + System.getProperty("BUILD_NUMBER", "0-SNAPSHOT"),
-      sbtVersion in Global := "0.13.5",
-      scalaVersion in Global := "2.10.2",
+      name         := "sbt-utc",
+      version      := "0.0." + System.getProperty("BUILD_NUMBER", "0-SNAPSHOT"),
+      sbtVersion   in Global := "0.13.6",
+      scalaVersion in Global := "2.10.4",
       libraryDependencies ++= lib_jodatime
-    ).settings(publishTo := Some("TIM Group Repo" at "http://repo.youdevise.com:8081/nexus/content/repositories/yd-release-candidates"))
-     .settings(credentials += Credentials(new File("/etc/sbt/credentials")))
+    )
+    .settings(
+      publishTo := Some("TIM Group Repo" at "http://repo.youdevise.com:8081/nexus/content/repositories/yd-release-candidates")
+    )
+    .settings(credentials += Credentials(new File("/etc/sbt/credentials")))
 }
